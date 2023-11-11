ca414e916437b6dc716c3c3f61ab9bd1c889aeb9
harelmarin <marin.harel@ynov.com>
2023-11-11 11:23:03 +0100

    TP fini

--- main.go
+++ main.go
@@ -7,8 +7,20 @@ import (
 	"os"
 )
 
+// Compteur de vue pour le challenge 2
+var CompteurVue int
+
+type UserData struct {
+	Nom    string
+	Prenom string
+	Date   string
+	Sexe   string
+}
+
+var userData UserData
+
 func main() {
-	//CHALLENGE 1-----------------------------------------------------------------
+	//CHALLENGE 1-----------------------------------------------------------------------------------------------
 	temp, err := template.ParseGlob("./templates/*.html")
 	if err != nil {
 		fmt.Println(fmt.Sprint("ERREUR => %s", err.Error()))
@@ -44,10 +56,54 @@ func main() {
 				{"Alan", "PHILIPIERT", true, 26}}}
 		temp.ExecuteTemplate(w, "var", data)
 	})
-	// CHALLENGE 1-------------------------------------------------------
+	// FIN CHALLENGE 1---------------------------------------------------------------------------------------------
 
-	// CHALLENGE 2--------------------------------------------------
+	// CHALLENGE 2--------------------------------------------------------------------------------------------
+	type Donnée struct {
+		Valeur int
+		Check  bool
+	}
 
+	http.HandleFunc("/change", func(w http.ResponseWriter, r *http.Request) {
+		CompteurVue++
+		var datachange = struct {
+			Valeur int
+			Check  bool
+		}{CompteurVue, CompteurVue%2 == 0}
+		temp.ExecuteTemplate(w, "change", datachange)
+	})
+	// FIN CHALLENGE 2--------------------------------------------------------------------------------------------------------
+
+	//CHALLENGE 3
+
+	// USER/INIT
+
+	http.HandleFunc("/user/init", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "init", nil)
+
+	})
+
+	// USER/TREATMENT
+	http.HandleFunc("/user/treatment", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			userData = UserData{
+				Nom:    r.FormValue("nom"),
+				Prenom: r.FormValue("prenom"),
+				Date:   r.FormValue("date"),
+				Sexe:   r.FormValue("sexe"),
+			}
+			http.Redirect(w, r, "/user/display", http.StatusSeeOther)
+		}
+		temp.ExecuteTemplate(w, "treatment", userData)
+	})
+
+	// USER/DISPLAY
+	http.HandleFunc("/user/display", func(w http.ResponseWriter, r *http.Request) {
+		datadisplay := userData
+		temp.ExecuteTemplate(w, "display", datadisplay)
+	})
+
+	/// FILE SERVER-----------------------------------------------------------------------------------------
 	rootDoc, _ := os.Getwd()
 	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
